cd2c77095a0aed89926662a4e3449c40f72c7e52
Bhavyesh Nial <bhavyeshnial@users.noreply.github.com>
2026-06-11 10:49:04 +0530

    live website

---
+++ includes/header.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Creative Design By Pooja</title>
+    <link rel="stylesheet" href="../assets/css/style.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" />
+</head>
+
+<body>
+    <header class="header" id="header">
+        <div class="container">
+            <div class="header-inner">
+                <!-- LOGO START -->
+                <div class="logo">
+                    <button type="button" class="menu-btn" aria-label="Open Menu" aria-expanded="false"
+                        aria-controls="mobile-menu">
+                        <i class="fa-solid fa-bars-staggered" aria-hidden="true"></i>
+                    </button>
+
+                    <a href="index.php" aria-label="Creative Design By Pooja Home">
+                        <img src="../assets/img/creativedesignsbypooja.png" alt="Creative Design By Pooja Logo"
+                            loading="eager" fetchpriority="high" />
+                    </a>
+                </div>
+                <!-- LOGO END -->
+                <!-- NAV START -->
+                <nav id="navbar" aria-label="Main Navigation">
+                    <ul class="nav-list">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link" aria-current="page">
+                                <i class="fa-solid fa-house" aria-hidden="true"></i>
+                                Home
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="#about-us" class="nav-link">
+                                <i class="fa-solid fa-user" aria-hidden="true"></i>
+                                About Us
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="#services" class="nav-link">
+                                <i class="fa-solid fa-gear" aria-hidden="true"></i>
+                                Services
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="#portfolio" class="nav-link">
+                                <i class="fa-solid fa-briefcase" aria-hidden="true"></i>
+                                Portfolio
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="#contact-us" class="nav-link">
+                                <i class="fa-solid fa-envelope" aria-hidden="true"></i>
+                                Contact Us
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="#cta" class="nav-cta nav-link">
+                                <i class="fa-solid fa-arrow-right-from-bracket" aria-hidden="true"></i>
+                                Get a Design
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <!-- NAV END -->
+                <!-- HEADER BTN START -->
+                <a href="#cta" class="header-btn" aria-label="Get a Design">
+                    <i class="fa-solid fa-arrow-right-from-bracket" aria-hidden="true"></i>
+                    Get a Design
+                </a>
+                <!-- HEADER BTN END -->
+            </div>
+        </div>
+    </header>
+    <script src="../assets/js/script.js"></script>
+</body>
+</html>
--- includes/main.php
+++ includes/main.php
@@ -1,12 +1,6 @@
 <?php
 // include 'coming-soon.php';
 ?>
-<?php
-// require './includes/main.php';
-?>
-<?php
-// include 'coming-soon.php';
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -67,7 +61,46 @@
         </div>
     </section>
    </main>
-
+   <!-- <div class="hero-wrapper">
+    <div class="hero-intro">
+        <h3>Designs That Make <br> Your Brand <br><span>Stand Out</span></h3>
+                    <p class="text">Logo,Packaging & Branding Solution <br>for Growing Business</p>
+                    <div class="hero-btn">
+                        <a href="" class="hero-cta">Start Your Project <i class="fa-solid fa-arrow-right-long"></i></a>
+                        <a href="" class="view-btn">View Portfolio</a>
+                    </div>
+                        <div class="hero-badge">
+                            <div class="badge-item">
+                                <i class="fa-solid fa-medal"></i>
+                                <span>Premium <br> Quality</span>
+                            </div>
+                            <div class="badge-item"> <i class="fa-solid fa-business-time"></i>
+                                <span>On-Time <br> Delevery</span>
+                            </div>
+                            <div class="badge-item">
+                                <i class="fa-solid fa-tag"></i>
+                                <span>Affordable <br>Pricing</span>
+                            </div>
+                        </div>
+    </div>
+    <div class="hero-img">
+        <div class="hero-banner"><img src="./assets/img/shiva.jpg" alt=""></div>
+        <div class="banner-wrappper">
+            <div class="banner-item">
+                <img src="./assets/img/shiva.jpg" alt="">
+            </div>
+            <div class="banner-item">
+                <img src="./assets/img/shiva.jpg" alt="">
+            </div>
+            <div class="banner-item">
+                <img src="./assets/img/shiva.jpg" alt="">
+            </div>
+            <div class="banner-item">
+                <img src="./assets/img/shiva.jpg" alt="">
+            </div>
+        </div>
+    </div>
+   </div> -->
 </body>
 
 </html>
