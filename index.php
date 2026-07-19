c9adadef76da0d9e7eb537adc6c68d2b4a7eeb08
Bhavyesh Nial <bhavyeshnial@users.noreply.github.com>
2026-07-20 00:42:26 +0530

    mobile

--- index.php
+++ index.php
@@ -3,8 +3,7 @@ require_once __DIR__ . '/config.php';
 include __DIR__ . '/includes/header.php';
 ?>
     <main>
-        <h1>git issue</h1>
-        <?= BASE_URL ?>
+
     <main/>
 <?php 
  include "./includes/footer.php";
