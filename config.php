b26538194c62148dd35a57488ec04de15e66065a
Bhavyesh Nial <bhavyeshnial@users.noreply.github.com>
2026-07-20 02:03:26 +0530

    refresh header and footer

--- config.php
+++ config.php
@@ -1,7 +1,13 @@
 <?php
 
-$isLocal = ($_SERVER['HTTP_HOST'] === 'localhost');
+$host = $_SERVER['HTTP_HOST'];
 
-define('BASE_URL',$isLocal ? '/designsbypooja/' :'/'); 
+$isLocal = (
+    $host === 'localhost' ||
+    $host === '127.0.0.1' ||
+    filter_var($host, FILTER_VALIDATE_IP)
+);
+
+define('BASE_URL', $isLocal ? '/designsbypooja/' : '/');
 
 ?>
